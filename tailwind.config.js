d24fcfd05885d0aef4cf2a054bfd2e0a888936d1
faiz bahu <faizbahu123@gmail.com>
2022-11-11 16:27:02 +0500

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        popup: "38px",
+        input_height: "67px",
+        input_width: "420px",
+        button_width: "202px",
+      },
+      colors: {
+        gray_dark: "#6C6F93",
+        gray_primary: "#9496b8",
+        gray_secondary: "#DEDFF7",
+        green: "#058346",
+        black: "#3d3d3d",
+      },
+    },
+  },
+  plugins: [],
+};
